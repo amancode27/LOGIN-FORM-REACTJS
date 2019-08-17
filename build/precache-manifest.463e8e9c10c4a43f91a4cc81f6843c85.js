6c0de667e0b7581f4ffa34723cc01e8a6792e9cb
amancode27 <43828332+amancode27@users.noreply.github.com>
2019-08-17 13:44:57 +0530

    commit second time

---
+++ build/precache-manifest.463e8e9c10c4a43f91a4cc81f6843c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6f4b4dffab1e226890aacd17d6fac0",
+    "url": "/LOGIN-FORM-REACTJS/index.html"
+  },
+  {
+    "revision": "a5b0994f1eb141dc7a03",
+    "url": "/LOGIN-FORM-REACTJS/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d7f09146a6a22b2aa35d",
+    "url": "/LOGIN-FORM-REACTJS/static/css/main.78ab39c4.chunk.css"
+  },
+  {
+    "revision": "a5b0994f1eb141dc7a03",
+    "url": "/LOGIN-FORM-REACTJS/static/js/2.840e4f0f.chunk.js"
+  },
+  {
+    "revision": "d7f09146a6a22b2aa35d",
+    "url": "/LOGIN-FORM-REACTJS/static/js/main.450431ca.chunk.js"
+  },
+  {
+    "revision": "31f7230c946d03c34c64",
+    "url": "/LOGIN-FORM-REACTJS/static/js/runtime~main.b00d403b.js"
+  }
+]);
